da50e5880d86748328d658767c958b29e0acd374
Sara Achieng <achieng997@gmail.com>
2024-07-15 14:33:33 +0300

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,1 +1,25 @@
 console.log('%c HI', 'color: firebrick')
+
+
+// function fetchImages() {
+//     fetch("https://dog.ceo/api/breeds/image/random/4")
+//     .then(resp => resp.json)
+//     .then(results => {
+//         results.message.forEach(image => renderImage(image))
+//     });
+
+// }
+
+// function renderImage(image) {
+//     const container = document.querySelector('#dog-image-container');
+
+//     const img = document.createElement('img');
+//     img.src = image;
+//     container.appendChild(img);
+
+// }
+
+
+// document.addEventListener('DOMContentLoaded', function() {
+//     fetchImages()
+// });
